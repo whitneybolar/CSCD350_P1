85b14b578dc6d188e8ce0757686e2a792c84397e
whitneybolar <43798491+whitneybolar@users.noreply.github.com>
2021-11-09 11:23:45 -0800

    Add files via upload

---
+++ p1_CSCD350/src/atcsim/loader/NavigationOverlayBuilder.java
@@ -0,0 +1,8 @@
+package atcsim.loader;
+
+public class NavigationOverlayBuilder {
+// In the constructor, add a print statement that says this is your code executing
+	public NavigationOverlayBuilder() {
+		System.out.println("Whitney's Code..");
+	}
+}
---
+++ p1_CSCD350/src/atcsim/loader/testerMain.java
@@ -0,0 +1,10 @@
+package atcsim.loader;
+
+public class testerMain {
+	public static void main(String[] args) {
+		NavigationOverlayBuilder navigationOverlayBuilder = new NavigationOverlayBuilder();
+		
+		// output
+		System.out.println(navigationOverlayBuilder);
+	}
+}
