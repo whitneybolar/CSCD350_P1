486cddb5eebc9dbfb7fe74bd15cbf39862f4b7a6
whitneybolar <43798491+whitneybolar@users.noreply.github.com>
2021-11-15 12:54:03 -0800

    Add files via upload

---
+++ p1_CSCD350/src/atcsim/world/A_ComponentWorld.java
@@ -0,0 +1,5 @@
+package atcsim.world;
+
+public class A_ComponentWorld<_SUBTYPE_ extends A_ComponentWorld<?>> {
+
+}
---
+++ p1_CSCD350/src/atcsim/world/navigation/A_ComponentNavaid.java
@@ -0,0 +1,15 @@
+package atcsim.world.navigation;
+
+import atcsim.world.A_ComponentWorld;
+
+public abstract class A_ComponentNavaid<_SUBTYPE_ extends A_ComponentWorld<?>> extends A_ComponentWorld<_SUBTYPE_>{
+
+//Gets the course to follow if this fix is designated as a hold or approach fix; otherwise, an assertion failure occurs.
+// Returns: the vector
+// public AngleNavigational getVector()
+
+
+// Gets whether this fix is designated as a hold or approach fix.
+// Returns: the result
+// public boolean hasVector()
+}
