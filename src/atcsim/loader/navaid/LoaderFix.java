cd9c82a027465ca74585ffb277d061715c2e1c3b
JThiessen <43521546+ChaosRecorded@users.noreply.github.com>
2021-11-19 12:11:55 -0800

    Add files via upload

--- src/atcsim/loader/navaid/LoaderFix.java
+++ src/atcsim/loader/navaid/LoaderFix.java
@@ -1,7 +1,21 @@
 package atcsim.loader.navaid;
 
-public class LoaderFix {
+import atcsim.loader.A_Loader;
+
+//Loads the definition of zero or more fix navaids, creates the corresponding navaids, and adds them to the given navigation overlay. 
+public class LoaderFix extends A_Loader{
 	
+	/*
+	//Constructor Creates a fix loader.
+	public LoaderFix(java.util.Map<java.lang.String,​A_ComponentNavaid<?>> navaids,OverlayNavigation overlay)
+	{
+		//Required Parameters
+		//    navaids - - the shared collection of navaids
+        //    overlay - - the navigation overlay that holds the navaids to be loaded
+	}*/
+
+
+	//Method Loads the fix section of the definition into the overlay.
 	public static void load​(java.util.Scanner scanner) throws java.io.IOException{
 		
 		System.out.println(scanner);
